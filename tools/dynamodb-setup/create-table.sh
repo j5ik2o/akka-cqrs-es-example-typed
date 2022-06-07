57e1d06e9710962abe4c8e844c37043837758c77
Junichi Kato <j5ik2o@gmail.com>
2022-06-08 08:33:56 +0900

    Changes

--- tools/dynamodb-setup/create-table.sh
+++ tools/dynamodb-setup/create-table.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # shellcheck disable=SC2046
 cd $(dirname "$0") && pwd
@@ -19,19 +19,17 @@ done
 
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-x}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-x}
+export AWS_PAGER=""
 
 if [[ $ENV_NAME = "prod" ]]; then
 
-AWS_PAGER="" \
-  aws dynamodb create-table \
+aws dynamodb create-table \
   --cli-input-json file://./journal-table.json
 
-AWS_PAGER="" \
-  aws dynamodb create-table \
+aws dynamodb create-table \
   --cli-input-json file://./snapshot-table.json
 
-#AWS_PAGER="" \
-#  aws dynamodb create-table \
+#aws dynamodb create-table \
 #  --cli-input-json file://./account-table.json
 
 else
@@ -39,18 +37,15 @@ else
 DYNAMODB_ENDPOINT=${DYNAMODB_ENDPOINT:-localhost:8000}
 echo "ENDPOINT = $DYNAMODB_ENDPOINT"
 
-AWS_PAGER="" \
-  aws dynamodb create-table \
+aws dynamodb create-table \
   --endpoint-url "http://$DYNAMODB_ENDPOINT" \
   --cli-input-json file://./journal-table.json
 
-AWS_PAGER="" \
-  aws dynamodb create-table \
+aws dynamodb create-table \
   --endpoint-url "http://$DYNAMODB_ENDPOINT" \
   --cli-input-json file://./snapshot-table.json
 
-#AWS_PAGER="" \
-#  aws dynamodb create-table \
+#aws dynamodb create-table \
 #  --endpoint-url "http://$DYNAMODB_ENDPOINT" \
 #  --cli-input-json file://./account-table.json
 
