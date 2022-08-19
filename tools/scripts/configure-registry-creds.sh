518fc2c03bdff2cf80e5e7fc90db156de927fac7
Junichi Kato <j5ik2o@gmail.com>
2022-08-19 22:19:33 +0900

    Update manifest files.

---
+++ tools/scripts/configure-registry-creds.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+minikube addons configure registry-creds
+minikube addons enable registry-creds
---
+++ tools/scripts/minikube-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+minikube start --kubernetes-version v1.21.0 --disk-size='10g'
