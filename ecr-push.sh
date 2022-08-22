c3dd9e26bacd6db60acbd4f70a4ce2c4974953de
Junichi Kato <j5ik2o@gmail.com>
2022-08-22 15:51:52 +0900

    Update ecr-push.sh

--- ecr-push.sh
+++ ecr-push.sh
@@ -2,4 +2,19 @@
 
 export TARGET_AMD64=1
 
-./sbt.sh write-api-server-scala/ecr:push
+set -eu
+
+# shellcheck disable=SC2046
+cd $(dirname "$0") || exit
+
+if [[ ! -e ./env.sh ]]; then
+    echo "env.sh is not found."
+    exit 1
+fi
+
+# shellcheck disable=SC2034
+OUTPUT_ENV=1
+
+. ./env.sh
+
+./sbt.sh write-api-server-"${MODE}"/ecr:push
