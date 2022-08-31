05428be1785f9514aaf1d0261fd26a9b79e1fb32
Junichi Kato <j5ik2o@gmail.com>
2022-08-31 11:50:13 +0900

    refactor docker-compose*.yml

--- tools/docker-compose/docker-compose-up.sh
+++ tools/docker-compose/docker-compose-up.sh
@@ -5,6 +5,15 @@ set -eu
 # shellcheck disable=SC2046
 cd $(dirname "$0") || exit
 
+F_OPTION="-f docker-compose-api.yml"
+
+while getopts d OPT; do
+  # shellcheck disable=SC2220
+  case ${OPT} in
+  "d") F_OPTION="" ;;
+  esac
+done
+
 if [[ ! -e ../../env.sh ]]; then
     echo "env.sh is not found."
     exit 1
@@ -16,4 +25,6 @@ OUTPUT_ENV=1
 source ../../env.sh
 
 ./docker-compose-down.sh && \
-  docker-compose up --force-recreate --remove-orphans --renew-anon-volumes "$@"
+  docker-compose \
+  -f docker-compose.yml $F_OPTION \
+  up --force-recreate --remove-orphans --renew-anon-volumes "$@"
