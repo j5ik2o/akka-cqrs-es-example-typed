2b73ce086590ced69f11703b24b95b848078a11b
Junichi Kato <j5ik2o@gmail.com>
2022-08-31 11:49:55 +0900

    refactor dynamodb-create-tables.sh.

--- tools/scripts/dynamodb-create-tables.sh
+++ tools/scripts/dynamodb-create-tables.sh
@@ -1,9 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -eu
 
 # shellcheck disable=SC2046
 cd $(dirname "$0") || exit
+
+# shellcheck disable=SC2039
+if [[ $# == 0 ]]; then
+  echo "Parameters are empty."
+  exit 1
+fi
+
+while getopts e: OPT; do
+  # shellcheck disable=SC2220
+  case ${OPT} in
+  "e") ENV_NAME="$OPTARG" ;;
+  esac
+done
 
 if [[ ! -e ../../env.sh ]]; then
     echo "env.sh is not found."
@@ -13,76 +26,8 @@ fi
 # shellcheck disable=SC2034
 OUTPUT_ENV=1
 
+# shellcheck disable=SC2039
 source ../../env.sh
 
-export AWS_DEFAULT_REGION=$AWS_REGION
-export AWS_ACCESS_KEY_ID=x
-export AWS_SECRET_ACCESS_KEY=x
-export AWS_PAGER=""
-
-DYNAMODB_ENDPOINT=localhost:31566
-JOURNAL_TABLE_NAME="${PREFIX}-Journal"
-JOURNAL_GSI_NAME="${PREFIX}-GetJournalRowsIndex"
-SNAPSHOT_TABLE_NAME="${PREFIX}-Snapshot"
-SNAPSHOT_GSI_NAME="${PREFIX}-GetSnapshotRowsIndex"
-
-echo "ENDPOINT = ${DYNAMODB_ENDPOINT}"
-
-aws dynamodb create-table \
-  --endpoint-url "http://$DYNAMODB_ENDPOINT" \
-  --table-name "${JOURNAL_TABLE_NAME}" \
-  --attribute-definitions \
-    AttributeName=pkey,AttributeType=S \
-    AttributeName=skey,AttributeType=S \
-    AttributeName=persistence-id,AttributeType=S \
-    AttributeName=sequence-nr,AttributeType=N \
-  --key-schema \
-    AttributeName=pkey,KeyType=HASH \
-    AttributeName=skey,KeyType=RANGE \
-  --provisioned-throughput \
-    ReadCapacityUnits=10,WriteCapacityUnits=10 \
-  --global-secondary-indexes \
-  "[
-    {
-      \"IndexName\": \"${JOURNAL_GSI_NAME}\",
-      \"KeySchema\": [{\"AttributeName\":\"persistence-id\",\"KeyType\":\"HASH\"},
-                      {\"AttributeName\":\"sequence-nr\",\"KeyType\":\"RANGE\"}],
-      \"Projection\":{
-        \"ProjectionType\":\"ALL\"
-      },
-      \"ProvisionedThroughput\": {
-        \"ReadCapacityUnits\": 10,
-        \"WriteCapacityUnits\": 10
-      }
-    }
-  ]" \
-  --stream-specification StreamEnabled=true,StreamViewType=NEW_IMAGE
-
-aws dynamodb create-table \
-  --endpoint-url "http://$DYNAMODB_ENDPOINT" \
-  --table-name "${SNAPSHOT_TABLE_NAME}" \
-  --attribute-definitions \
-    AttributeName=pkey,AttributeType=S \
-    AttributeName=skey,AttributeType=S \
-    AttributeName=persistence-id,AttributeType=S \
-    AttributeName=sequence-nr,AttributeType=N \
-  --key-schema \
-    AttributeName=pkey,KeyType=HASH \
-    AttributeName=skey,KeyType=RANGE \
-  --provisioned-throughput \
-    ReadCapacityUnits=10,WriteCapacityUnits=10 \
-  --global-secondary-indexes \
-  "[
-    {
-      \"IndexName\": \"${SNAPSHOT_GSI_NAME}\",
-      \"KeySchema\": [{\"AttributeName\":\"persistence-id\",\"KeyType\":\"HASH\"},
-                      {\"AttributeName\":\"sequence-nr\",\"KeyType\":\"RANGE\"}],
-      \"Projection\":{
-        \"ProjectionType\":\"ALL\"
-      },
-      \"ProvisionedThroughput\": {
-        \"ReadCapacityUnits\": 10,
-        \"WriteCapacityUnits\": 10
-      }
-    }
-  ]"
+# shellcheck disable=SC2039
+source ../dynamodb-setup/create-tables.sh -e $ENV_NAME
