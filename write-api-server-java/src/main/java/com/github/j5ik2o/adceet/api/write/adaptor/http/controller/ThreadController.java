a438645f0b8879380a34535b5a26d890f03ac587
Junichi Kato <j5ik2o@gmail.com>
2022-09-07 08:56:47 +0900

    wip

--- write-api-server-java/src/main/java/com/github/j5ik2o/adceet/api/write/adaptor/http/controller/ThreadController.java
+++ write-api-server-java/src/main/java/com/github/j5ik2o/adceet/api/write/adaptor/http/controller/ThreadController.java
@@ -15,6 +15,87 @@
  */
 package com.github.j5ik2o.adceet.api.write.adaptor.http.controller;
 
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.AllDirectives;
 
-public class ThreadController extends AllDirectives {}
+import static akka.http.javadsl.server.PathMatchers.segment;
+
+import akka.http.javadsl.server.Route;
+import com.github.j5ik2o.adceet.api.write.JacksonObjectMappers;
+import com.github.j5ik2o.adceet.api.write.adaptor.http.json.CreateThreadRequestJson;
+import com.github.j5ik2o.adceet.api.write.adaptor.http.json.CreateThreadResponseJson;
+import com.github.j5ik2o.adceet.api.write.adaptor.http.validation.ValidationRejection;
+import com.github.j5ik2o.adceet.api.write.adaptor.http.validation.Validator;
+import com.github.j5ik2o.adceet.api.write.domain.ThreadId;
+import com.github.j5ik2o.adceet.api.write.use_case.CreateThreadUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+
+public final class ThreadController extends AllDirectives {
+  private final CreateThreadUseCase createThreadUseCase;
+
+  public ThreadController(CreateThreadUseCase createThreadUseCase) {
+    this.createThreadUseCase = createThreadUseCase;
+  }
+
+  public Route toRoute() {
+    return concat(createThread());
+  }
+
+  @Path("/threads")
+  @POST
+  @Operation(
+      description = "Create a thread",
+      method = "POST",
+      requestBody =
+          @RequestBody(
+              required = true,
+              content =
+                  @Content(
+                      schema = @Schema(implementation = CreateThreadRequestJson.class),
+                      mediaType = MediaType.APPLICATION_JSON)),
+      responses =
+          @ApiResponse(
+              responseCode = "200",
+              description = "OK",
+              content =
+                  @Content(
+                      schema = @Schema(implementation = CreateThreadResponseJson.class),
+                      mediaType = MediaType.APPLICATION_JSON)))
+  public Route createThread() {
+    return path(
+        "thread",
+        () ->
+            post(
+                () ->
+                    entity(
+                        Jackson.unmarshaller(
+                            JacksonObjectMappers.defaultObjectMapper(),
+                            CreateThreadRequestJson.class),
+                        json ->
+                            Validator.validateAccountId(json.accountId())
+                                .fold(
+                                    errors -> reject(new ValidationRejection(errors)),
+                                    accountId -> {
+                                      var result =
+                                          createThreadUseCase.execute(new ThreadId(), accountId);
+                                      return onSuccess(
+                                          result,
+                                          threadId ->
+                                              complete(
+                                                  StatusCodes.OK,
+                                                  new CreateThreadResponseJson(threadId.asString()),
+                                                  Jackson.marshaller(
+                                                      JacksonObjectMappers.defaultObjectMapper())));
+                                    }))));
+  }
+}
