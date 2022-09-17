e268dd670992467de75805ac9dd151a410a30d4d
Junichi Kato <j5ik2o@gmail.com>
2022-09-18 08:54:07 +0900

    wip

--- write-api-server-java/src/main/java/com/github/j5ik2o/adceet/api/write/MainActorFactory.java
+++ write-api-server-java/src/main/java/com/github/j5ik2o/adceet/api/write/MainActorFactory.java
@@ -15,16 +15,54 @@
  */
 package com.github.j5ik2o.adceet.api.write;
 
+import akka.Done;
+import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.cluster.typed.Cluster;
+import akka.http.javadsl.Http;
+import com.github.j5ik2o.adceet.api.write.adaptor.http.Routes;
+import com.github.j5ik2o.adceet.api.write.adaptor.http.controller.ThreadController;
 import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import wvlet.log.io.StopWatch;
 
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
+
 public final class MainActorFactory {
   private static final Logger LOGGER = LoggerFactory.getLogger(MainActorFactory.class);
+
+  static CompletionStage<Done> startHttpServer(
+      Injector injector,
+      ActorSystem<Void> system,
+      String host,
+      int port,
+      Duration terminationHardDeadLine) {
+    var threadController = injector.getInstance(Key.get(new TypeLiteral<ThreadController>() {}));
+    var http =
+        Http.get(system)
+            .newServerAt(host, port)
+            .bind(new Routes(threadController).toRoute())
+            .thenApply(it -> it.addToCoordinatedShutdown(terminationHardDeadLine, system))
+            .whenComplete(
+                (serverBinding, throwable) -> {
+                  if (serverBinding != null) {
+                    var address = serverBinding.localAddress();
+                    LOGGER.info(
+                        "server bound to http://%s:%d"
+                            .formatted(address.getHostString(), address.getPort()));
+                  }
+                  if (throwable != null) {
+                    LOGGER.error("Failed to bind endpoint, terminating system", throwable);
+                    system.terminate();
+                  }
+                });
+    return http.thenApply(ignore -> Done.getInstance());
+  }
 
   public static Behavior<MainProtocol.Command> create(Injector injector, StopWatch stopWatch) {
     return Behaviors.setup(
@@ -39,6 +77,15 @@ public final class MainActorFactory {
 
           LOGGER.info("selfMember.roles = {}", selfMember.getRoles());
           var roleNames = RoleNames.from(selfMember);
+
+          var config = ctx.getSystem().settings().config();
+          var adceetConfig = config.getConfig("adceet");
+          var host = adceetConfig.getString("http.host");
+          var port = adceetConfig.getInt("http.port");
+          var terminationHardDeadLine =
+              adceetConfig.getDuration("management.http.termination-hard-deadline");
+          var loadBalancerDetachWaitDuration =
+              adceetConfig.getDuration("management.http.load-balancer-detach-wait-duration");
 
           var childInjector = injector.createChildInjector(new MainActorModule(ctx, roleNames));
           // childInjector.getInstance()
