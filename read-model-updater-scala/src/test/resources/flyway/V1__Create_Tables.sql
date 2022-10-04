27745a6e1c37dbff550f1074c3f0071c3991e75e
Junichi Kato <j5ik2o@gmail.com>
2022-10-04 09:20:56 +0900

    wip

--- read-model-updater-scala/src/test/resources/flyway/V1__Create_Tables.sql
+++ read-model-updater-scala/src/test/resources/flyway/V1__Create_Tables.sql
@@ -25,7 +25,7 @@ CREATE TABLE `messages`
     `id`         varchar(64) NOT NULL,
     `thread_id`  varchar(64) NOT NULL,
     `account_id` varchar(64) NOT NULL,
-    `text`       TEXT NOT NULL,
+    `text`       TEXT        NOT NULL,
     `created_at` TEXT        NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`thread_id`) REFERENCES threads (`id`)
