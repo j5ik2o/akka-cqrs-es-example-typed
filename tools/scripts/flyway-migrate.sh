c1870ab743418a1f8991210cc36f57565b2bb8ab
Junichi Kato <j5ik2o@gmail.com>
2022-10-13 18:03:08 +0900

    add impls for local k8s

---
+++ tools/scripts/flyway-migrate.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -eu
+
+# shellcheck disable=SC2046
+cd $(dirname "$0") || exit
+
+# shellcheck disable=SC2039
+if [[ $# == 0 ]]; then
+  echo "Parameters are empty."
+  exit 1
+fi
+
+while getopts e: OPT; do
+  # shellcheck disable=SC2220
+  case ${OPT} in
+  "e") ENV_NAME="$OPTARG" ;;
+  esac
+done
+
+if [[ ! -e ../../env.sh ]]; then
+    echo "env.sh is not found."
+    exit 1
+fi
+
+# shellcheck disable=SC2034
+OUTPUT_ENV=1
+
+# shellcheck disable=SC2039
+source ../../env.sh
+
+docker run --rm \
+  -v $(pwd)/flyway/sql:/flyway/sql \
+  -v $(pwd)/flyway/drivers:/flyway/drivers \
+  flyway/flyway \
+  -url='jdbc:mysql://host.docker.internal:30306/adceet?allowPublicKeyRetrieval=true&useSSL=false' -user=root -password=passwd migrate -X
